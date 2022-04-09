2af55fdb91f2ba33e48a7aa1b73584978375034c
XRLsc <83329088+XRLsc@users.noreply.github.com>
2022-04-09 08:55:52 -0700

    Update v0.7

--- renderer/index.js
+++ renderer/index.js
@@ -1,6 +1,6 @@
 const { ipcRenderer, shell } = require('electron')
 const fetch = require('node-fetch')
-const currentv = "0.6"
+const currentv = "0.7"
 
 document.getElementById('connect').addEventListener('click', () => {
   document.getElementById('content').innerHTML = "Connecting..."
